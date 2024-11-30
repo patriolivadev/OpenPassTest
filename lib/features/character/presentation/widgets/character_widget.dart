8f0eca8841441f75ed6933b5f2aa18b0d1e8d046
Patricio Oliva <patrioliva2015@gmail.com>
2024-11-30 11:35:49 -0300

    Se refactorizó CharacterWidget.

--- lib/features/character/presentation/widgets/character_widget.dart
+++ lib/features/character/presentation/widgets/character_widget.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:open_pass_test_oliva_patricio/core/services/dependencies_service.dart';
 import 'package:open_pass_test_oliva_patricio/features/character/domain/entities/character.dart';
 import 'package:open_pass_test_oliva_patricio/features/character/presentation/manager/character_bloc.dart';
 
-class CharacterWidget extends StatefulWidget {
+class CharacterWidget extends StatelessWidget {
   final Character character;
 
   const CharacterWidget({
@@ -13,88 +12,74 @@ class CharacterWidget extends StatefulWidget {
   });
 
   @override
-  State<CharacterWidget> createState() => _CharacterWidgetState();
-}
-
-class _CharacterWidgetState extends State<CharacterWidget> {
-  final CharacterBloc _bloc = getIt<CharacterBloc>();
-  List<int> favorites = [];
-
-  @override
   Widget build(BuildContext context) {
-    return BlocConsumer<CharacterBloc, CharacterState>(
-      bloc: _bloc,
-      listener: listener,
-      builder: builder,
-    );
-  }
+    final CharacterBloc bloc = getIt<CharacterBloc>();
 
-  listener(context, state) {}
-
-  Widget builder(context, state) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.03),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      widget.character.name,
-                      style: const TextStyle(fontSize: 25),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(
-                    widget.character.isFavorite
-                        ? Icons.star
-                        : Icons.star_border,
-                    color: widget.character.isFavorite ? Colors.yellow : null,
-                    size: 40,
-                  ),
-                  onPressed: () => toggleFavorite(widget.character),
-                ),
-              ],
-            ),
-          ),
+          _buildHeader(context, bloc),
           const SizedBox(height: 8),
+          _buildCharacterImage(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context, CharacterBloc bloc) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.03),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
           Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(
-                getCharacterImageUrl(widget.character.id),
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) =>
-                    const Icon(Icons.image, size: 10),
+            child: Center(
+              child: Text(
+                character.name,
+                style: const TextStyle(fontSize: 25),
+                overflow: TextOverflow.ellipsis,
               ),
             ),
+          ),
+          IconButton(
+            icon: Icon(
+              character.isFavorite ? Icons.star : Icons.star_border,
+              color: character.isFavorite ? Colors.yellow : null,
+              size: 40,
+            ),
+            onPressed: () => _toggleFavorite(bloc),
           ),
         ],
       ),
     );
   }
 
-  String getCharacterImageUrl(int id) {
-    String url =
-        'https://starwars-visualguide.com/assets/img/characters/$id.jpg';
-    return url;
+  Widget _buildCharacterImage() {
+    return Expanded(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.network(
+          _getCharacterImageUrl(),
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) =>
+              const Icon(Icons.image, size: 50),
+        ),
+      ),
+    );
   }
 
-  void toggleFavorite(Character character) {
-    setState(() {
-      if (widget.character.isFavorite) {
-        _bloc.add(ActionRemoveFavoriteCharacter(id: character.id));
-      } else {
-        _bloc.add(ActionSaveFavoriteCharacter(character: character));
-      }
-    });
+  String _getCharacterImageUrl() {
+    return 'https://starwars-visualguide.com/assets/img/characters/${character.id}.jpg';
+  }
+
+  void _toggleFavorite(CharacterBloc bloc) {
+    if (character.isFavorite) {
+      bloc.add(ActionRemoveFavoriteCharacter(id: character.id));
+    } else {
+      bloc.add(ActionSaveFavoriteCharacter(character: character));
+    }
   }
 }
